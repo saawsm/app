7183ff00f613968503ab64adcd674925e156b389
saawsm <selectiveunknown@hotmail.com>
2020-03-27 21:54:06 +1100

    Init commit.

---
+++ App.Tests/Tests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace Saawsm.App.Tests {
+    public class Tests {
+
+        [Fact]
+        public void DummyTest() {
+            Assert.True(2 * 2 == 4);
+        }
+
+    }
+}
---
+++ App/FormMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Saawsm.App {
+
+    public partial class FormMain : Form {
+        public FormMain() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Text = $"{Application.ProductName} v{Application.ProductVersion}";
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            MessageBox.Show("Hello World", "Basic MSG Box");
+        }
+    }
+
+}
